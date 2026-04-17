169c8b8c7ca10dce5fccbe983226cf4a32367ec0
Bob Green <bob@chippers.org.uk>
2026-04-17 14:54:13 +0100

    - Housekeeping: Removed unneeded files - fixed 'get_cpu_type()'. Should work for 68000 through to 68030. - Modified pre_main() to detect and display the CPU type.

--- src/lib/machine/_misc.asm
+++ src/lib/machine/_misc.asm
@@ -15,37 +15,48 @@ ill_inst_vec    equ     4<<2
 ;    3: 68030
 ;
 detect_cpu_type::
-                movem.l d1/d2/a0,-(sp)
+                movem.l d1/d2/d3/a0/a2,-(sp)
+
                 move.l  #0,a0
                 move.l  ill_inst_vec(a0),d1     ; Temporarily take over the illegal
                 move.l  d1,saved_vector         ; instruction handler.
-                move.l  illegal_inst,d1
-                move.l  d1,ill_inst_vec(a0)
+
+                lea     illegal_inst,a2
+                movea.l a2,ill_inst_vec(a0)
+
                 move.l  sp,a2                   ; Remember the stack pointer
 
-                moveq.l #0,d0                   ; Default is 68000/68008
+                move.b  #0,d3
+                moveq.l #0,d0                   ; Default is 68000/68008 (0)
 
 ; Now try executing instructions for each processor in ascending order until one fails
-                move    ccr,d2                  ; Needs 68010
+                move    ccr,d2                  ; Needs 68010 (1)
+                tst.b   d3                      ; Did an exception take place?
+                bne     cpu_detected
                 addi.l  #1,d0
 
-                movec   cacr,d2                 ; Needs 68020
+                movec   cacr,d2                 ; Needs 68020 (2)
+                tst.b   d3                      ; Did an exception take place?
+                bne     cpu_detected
                 addi.l  #1,d0
 
-                dc.l    $4E7A0807               ; MOVEC SRP,D0 - 68030+
+                pmove   tc,saved_tt0            ; Needs a 68030 (3)
+                tst.b   d3                      ; Did an exception take place?
+                bne     cpu_detected
                 addi.l  #1,d0
 
 cpu_detected
-                move.l  #0,a0                   ; Restore the original illegal
                 move.l  saved_vector,d1         ; instruction handler
                 move.l  d1,ill_inst_vec(a0)     
-                movem.l (sp)+,d1/d2/a0
+
+                movem.l (sp)+,d1/d2/d3/a0/a2
                 rts
 
 illegal_inst:
-                move.l  a2,sp
-                bra     cpu_detected
+                move.b  #$ff,d3
+                rte
 
                 section .data,data
 
 saved_vector    dc.l    0
+saved_tt0       dc.l    0
--- src/lib/machine/pre_main.c
+++ src/lib/machine/pre_main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <machine.h>
 
+uint8_t cpu_type = CPU_68000;
+
+static const char *cpus[] = {
+    "68000/68008",
+    "68010",
+    "68020",
+    "68030 Enhanced 32-bit"
+};
+
 void pre_main(void) {
+    printf("Mega-68030 Computer System\n");
     printf("C-Runtime initialising. Code is running in %s\n", running_in_rom?"ROM":"RAM");
+    
+    cpu_type = (uint8_t) detect_cpu_type();
+    if (cpu_type > 3) {
+        printf("UNKOWN processor type detected\n");
+    }
+    else {
+        printf("%s Processor\n", cpus[cpu_type]);
+    }
 }
